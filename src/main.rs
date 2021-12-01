5118dc7cc6ed8db8dc0c2d70c4da08db42aedfd1
Alexander Senier <mail@senier.net>
2021-12-02 00:25:46 +0100

    Load passwords from database

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, Builder, CellRendererText, Label, TreeView, TreeViewColumn, ListStore};
+use keepass::{Database, NodeRef};
+use std::fs::File;
+use std::error::Error;
 
-fn create_and_fill_model() -> ListStore {
-    let model = ListStore::new(&[String::static_type()]);
+fn create_model(file: &str, password: &str) -> Result<ListStore, Box<dyn Error>> {
+    let db = Database::open(
+        &mut File::open(std::path::Path::new(file))?,
+        Some(password),
+        None
+    )?;
 
-    let entries = &["e1", "e2", "e3", "e4", "e5", "e6"];
-    for entry in entries.iter() {
-        model.insert_with_values(None, &[(0, &entry)]);
+    let result = ListStore::new(&[String::static_type()]);
+
+    for node in &db.root {
+        match node {
+            NodeRef::Group(_) => {},
+            NodeRef::Entry(e) => {
+        		result.insert_with_values(None, &[(0, &e.get_title().unwrap())]);
+            }
+        }
     }
-    model
+
+    Ok(result)
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>>{
     let kqpr = Application::builder()
         .application_id("net.senier.kqpr")
         .build();
@@ -20,7 +34,8 @@ fn main() {
 
         let builder: Builder = Builder::from_string(include_str!("ui.glade"));
         let view: TreeView = builder.object("tree_entries").expect("Tree view not found");
-        let model: ListStore = create_and_fill_model();
+
+        let model = create_model("tests/data/Passwords.kdbx", "demopass");
         let window: ApplicationWindow = builder.object("window_main").expect("Window not found");
 
         window.set_application(Some(application));
@@ -34,7 +49,7 @@ fn main() {
         column.add_attribute(&cell, "text", 0);
         view.append_column(&column);
 
-		view.set_model(Some(&model));
+		view.set_model(Some(&model.expect("Error loading DB")));
         view.connect_cursor_changed(move |tree_view| {
             let selection = tree_view.selection();
             if let Some((model, iter)) = selection.selected() {
@@ -44,4 +59,5 @@ fn main() {
     });
 
     kqpr.run();
+    Ok(())
 }
